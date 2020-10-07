085b8b8ac0dcdb02d6c72447b8cfb210a3390314
Martin Piper <martin.piper@gmail.com>
2020-10-08 00:51:01 +0800

    Added option to write debug frames

--- src/main/java/com/bdd6502/TestRunner.java
+++ src/main/java/com/bdd6502/TestRunner.java
@@ -65,6 +65,7 @@ public class TestRunner {
     public static void main(String args[]) throws Exception {
         if (args.length >= 1 && args[0].compareToIgnoreCase("--execVideoTest") == 0) {
             DisplayBombJack displayBombJack = new DisplayBombJack();
+//            displayBombJack.writeDebugBMPsToLeafFilename("target/frames/execVideoTest-");
             displayBombJack.enableDebugData();
             displayBombJack.addLayer(new Mode7(0xa000, 0x08));
             displayBombJack.addLayer(new Tiles(0x9e00, 0x80, 0x40));
